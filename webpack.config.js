e52b9993143d9bff89553ac51bc1e53bc707a950
fr02t1 <rebelwatt@cock.li>
2024-08-25 12:00:10 +0200

    add typescript support to Babel and ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = (env, argv) => ({
   entry: [
     'whatwg-fetch',
-    './src/index.js'
+    './src/index.tsx' // Update entry point if your main file is now a TypeScript file
   ],
   devServer: {
     static: path.resolve(__dirname, 'dist'),
@@ -13,10 +13,18 @@ module.exports = (env, argv) => ({
   module: {
     rules: [
       {
+        test: /\.tsx?$/, // Add support for .ts and .tsx files
+        exclude: /node_modules/,
+        use: 'ts-loader'
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: 'babel-loader'
       }
     ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'] // Add .ts and .tsx as resolvable extensions
   }
-})
+});
